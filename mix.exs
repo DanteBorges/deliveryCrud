3381730924ad6de0b8e0cd6c7a44f2031bd2c01e
Daniel <danielnorges@gmail.com>
2023-09-17 20:50:55 -0300

    feat: install excoveralls

--- mix.exs
+++ mix.exs
@@ -1,16 +1,22 @@
 defmodule DeliveryCrud.MixProject do
   use Mix.Project
-
   def project do
     [
       app: :deliveryCrud,
       version: "0.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -44,7 +50,7 @@ defmodule DeliveryCrud.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-
+      {:excoveralls, "~> 0.10", only: :test},
     ]
   end
 
