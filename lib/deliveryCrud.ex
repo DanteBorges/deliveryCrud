eb0d7193b96f7853a09fcfc1ac3f558c225785aa
Daniel <danielnorges@gmail.com>
2023-09-17 20:14:20 -0300

    feat: add get users in controller

--- lib/deliveryCrud.ex
+++ lib/deliveryCrud.ex
@@ -1,7 +1,10 @@
 defmodule DeliveryCrud do
 
   alias DeliveryCrud.Users.Create, as: UserCreate
+  alias DeliveryCrud.Users.Get, as: UserGet
+
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 
 end
 DeliveryCrud.create_user()
